19b5e29fb3da264cc49c2d3d6b79bc729dcaee7e
Frank Lavery <frank.lavery@westnet.com.au>
2022-03-13 17:34:54 +0800

    js functioning correctly except highscores screen change of class

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,27 +1,27 @@
 // javascript file containing quiz questions and answers
 
 const quiz = [{
-  qu: "How many roads must a man walk down?",
-  answers: ["Who knows", "depends, gravel or bitumen", "depends on the man", "7"],
-  correct: 3
-}, {
-  qu: "How do you find the highest value of x and y?",
-  answers: ["Math.max(x,y)", "ceil(x,y)", "Math.ceil(x,y)", "top(x,y)"],
-  correct: 0
+    question: "How many roads must a man walk down?",
+    answers: ["Who knows", "depends, gravel or bitumen", "depends on the man", "7"],
+    correct: 3
   }, {
-    qu: "Which operator is used to assign a value to a variable?",
+    question: "How do you find the highest value of x and y?",
+    answers: ["Math.max(x,y)", "ceil(x,y)", "Math.ceil(x,y)", "top(x,y)"],
+    correct: 0
+  }, {
+    question: "Which operator is used to assign a value to a variable?",
     answers: ["*", "=", "-", "X"],
     correct: 1
   }, {
-    qu: "Commonly used data types DO NOT include:",
+    question: "Commonly used data types DO NOT include:",
     answers: ["strings", "booleans", "alerts", "numbers"],
     correct: 2
   }, {
-    qu: "The conditions in an if/else statement is enclosed within ___.",
+    question: "The conditions in an if/else statement is enclosed within ___.",
     answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
     correct: 2
   }, {
-    qu: "Arrays in JavaScript can be used to store ___.",
+    question: "Arrays in JavaScript can be used to store ___.",
     answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     correct: 3
-  }]
+}]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,246 +1,236 @@
 // Variables for accessing html
-const startBtn = document.getElementById("start-button");
-const highscoreBtn = document.getElementById("high-score");
-const submitBtn = document.getElementById("submit");
-const goBackBtn = document.getElementById("go-back");
-const clearScoreBtn = document.getElementById("clear-scores");
-const timerText = document.getElementById("timer-display");
-const startScrn = document.getElementById("start-screen");
-const questionScrn = document.getElementById("question-screen");
-const gameoverScrn = document.getElementById("game-over");
-const highScoreScrn = document.getElementById("high-score");
-const finalScoreText = document.getElementById("final-score");
-const questionAsked = document.querySelector(".question-asked");
-const answerList = document.querySelector("#answer-list");
-const questionResult = document.querySelector("#result");
-const initials = document.querySelector('#initials');
-const scoreList = document.querySelector('#score-list');
+const btnStart = document.getElementById("start-button");
+const btnHighScores = document.getElementById("high-score");
+const btnSubmit = document.getElementById("submit");
+const btnGoBack = document.getElementById("go-back");
+const btnClearHighScores = document.getElementById("clear-scores");
+const scrnStart = document.getElementById("start-screen");
+const scrnQuestions = document.getElementById("question-screen");
+const scrnGameOver = document.getElementById("game-over");
+const scrnHighScore = document.getElementById("high-score");
+const txtTimer = document.getElementById("timer-display");
+const txtFinalScore = document.getElementById("final-score");
+const txtAskQuestion = document.querySelector(".question-asked");
+const lstAnswers = document.querySelector("#answer-list");
+const txtResult = document.querySelector("#result");
+const inptInitials = document.querySelector('#initials');
+const lstScoreList = document.querySelector('#score-list');
 
 // Global variables and assignments
 let timer;
 let timeRemaining;
 let finalScore = 0;
-let questionIndex;
+let idxQuestion;
 const savedScores = [];
 
-function checkAnswer(event) {
-  event.preventDefault();
-  const correctAnswer = event.target.getAttribute('correct') === 'true';
-  if (correctAnswer) {
-    questionResult.textContent="Correct!"
-  } else {
-    timeRemaining = timeRemaining - 10;
-    questionResult.textContent = "Wrong!";
-  }
-  // move to next question
-  questionIndex = questionIndex + 1;
-  if (questionIndex < quiz.length) {
-    getQuestions(questionIndex);
-  } else {
-    // when all questions answered correctly, trigger win condition
-    questionIndex = questionIndex + 1;
+// Functions that start and run quiz
+// Function to retrieve list of saved scores from local storage
+function getSavedScores() {
+  // get high scores from local storage
+  let currentScoreList = JSON.parse(localStorage.getItem("storedScores"));
+  // populate savedScores array from data in local storage
+  if (currentScoreList != null) {
+    // add each name/score object in storage to savedScores
+    for (i = 0; i < currentScoreList.length; i++) {
+      savedScores[i] = currentScoreList[i];
+    }
   }
   return;
 }
 
+// Function to add name/score to local storage and reload the page
+function writeScore(event) {
+  event.preventDefault();
+  // submit Initials and score to local storage
+  let currentName = inptInitials.value.trim();
+  // if initials are entered, retrieve saved scores from local storage and add current Name
+  if (currentName != "" || null) {
+    getSavedScores();
+    // create object with player name/score and add to list of saved scores
+    const tempObj = {
+      saveName: currentName,
+      saveScore: finalScore,
+    }
+    savedScores.push(tempObj);
+    // write updated high score list to local storage
+    localStorage.setItem("storedScores", JSON.stringify(savedScores));
+  }
+  window.location.reload();
+  return;
+}
+
+// Function to show the User's score and allow entry of initials
+function endQuiz() {
+  // display players final score
+  txtFinalScore.textContent = finalScore;
+  // hide question screen
+  scrnQuestions.classList.add("hide");
+  // show Gameover screen - allows user to enter Initials
+  scrnGameOver.classList.remove("hide");
+  btnSubmit.addEventListener("click", writeScore);
+  return;
+}
+
+// Function to start and end timer when end conditions met
 function startTimer() {
-  // set interval and start Timer
+  // set interval and start Timer with interval 1000ms
   timer = setInterval(function () {
     timeRemaining--;
-    timerText.textContent = timeRemaining;
- 
-    // check if all questions answered correctly
-    if (questionIndex > quiz.length) {
+    // end condition if all questions answered
+    if (idxQuestion > quiz.length) {
       // stop timer and set final score to time remaining
       clearInterval(timer);
       finalScore = timeRemaining;
-      // go to final score screen
+      // go to Game Over screen
       endQuiz();
     }
-    // condition if player runs out of time
+    // end condition if User runs out of time
     if (timeRemaining <= 0) {
       // stop timer and set final score to zero
       clearInterval(timer);
       timeRemaining = 0;
-      timerText.textContent = timeRemaining;
       finalScore = 0;
-      // go to final score screen
+      // go to Game Over screen
       endQuiz();
     }
+    // display time remaining on screen
+    txtTimer.textContent = timeRemaining;
   }, 1000);
-}
-
-function quizQuestions() {
-  answerList.textContent = "";
-    // render current question and answers
-    const question = quiz[questionIndex];
-    questionAsked.textContent = question.qu;
-    for (var i = 0; i < question.answers.length; i++) {
-      // render answers as a list of buttons
-      const answer = question.answers[i];
-      const li = document.createElement('li');
-      const button = document.createElement('button');
-      button.textContent = answer;
-      // add button attribute to identify correct answer
-      if (question.correct == i) {
-        button.setAttribute('correct', true);
-      } else {
-        button.setAttribute('correct', false);
-      }
-      button.addEventListener('click', checkAnswer);
-      li.appendChild(button);
-      answerList.append(li);
-    } 
   return;
-  }
-
-function getQuestions(questionIndex) {
-  quizQuestions(questionIndex);
-return;
 }
 
+// Function to check User's answer and call the next question
+function checkAnswer(event) {
+  event.preventDefault();
+  // check answer
+  const correctAnswer = event.target.getAttribute('correct') === 'true';
+  if (correctAnswer) {
+    txtResult.textContent = "Correct!"
+  } else {
+    timeRemaining = timeRemaining - 10;
+    txtResult.textContent = "Wrong!";
+  }
+  // move to next question
+  idxQuestion = idxQuestion + 1;
+  if (idxQuestion < quiz.length) {
+    quizQuestions(idxQuestion);
+  } else {
+    // when all questions answered, trigger end condition in startTimer function
+    idxQuestion = idxQuestion + 1;
+  }
+  return;
+}
+
+// Function to display the quiz question and answer options
+function quizQuestions(idxQuestion) {
+  // reset the answer list
+  lstAnswers.textContent = "";
+  // render current question and answers
+  const currentQuestion = quiz[idxQuestion];
+  txtAskQuestion.textContent = currentQuestion.question;
+  for (var i = 0; i < currentQuestion.answers.length; i++) {
+    // render answers as a list of buttons
+    const currentAnswer = currentQuestion.answers[i];
+    const li = document.createElement('li');
+    li.classList.add("list-answer");
+    const button = document.createElement('button');
+    button.classList.add("btn-answer");
+    button.textContent = currentAnswer;
+    // add button attribute to identify if current answer is correct
+    if (currentQuestion.correct === i) {
+      button.setAttribute('correct', true);
+    } else {
+      button.setAttribute('correct', false);
+    }
+    li.appendChild(button);
+    lstAnswers.append(li);
+    // add listener to determine if current answer is selected
+    button.addEventListener('click', checkAnswer);
+  }
+  return;
+}
+
+// Function to reset global variables and start the quiz
+function beginQuiz() {
+  // reset variables
+  txtResult.textContent = "";
+  inptInitials.textContent = "";
+  timeRemaining = 70;
+  txtTimer.textContent = timeRemaining;
+  idxQuestion = 0;
+  savedScores.length = 0;
+
+  // disable High Scores button while quiz is in progress
+  btnHighScores.disabled = true;
+
+  // hide startup screen
+  scrnStart.classList.add("hide");
+  // display question screen
+  scrnQuestions.classList.remove("hide");
+  // start timer and display first question
+  startTimer();
+  quizQuestions(idxQuestion);
+  return;
+}
+
+// Function to display high scores to the screen as a list
 function renderHighScores() {
-  console.log("in RenderHighScores");
-
-  let tempScoreName = "";
-  let tempHighScore;
-
+  // set maximum number of high scores to show
+  let numToDisplay = 5;
+  // prevent multiple clicks of Highscores button from adding multiple sets of the high scores list
+  lstScoreList.innerHTML = "";
+  // call function to sort the high scores list
+  savedScores.sort(function (a, b) {
+    if (b.saveScore > a.saveScore) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+  // creates and renders high scores if there are any saved
   if (savedScores.length != 0) {
-    for (i = 0; i < savedScores.length; i++) {
-      tempScoreName = savedScores[i].saveName;
-      tempHighScore = savedScores[i].saveScore;
-
+    // limit the display to length fo savedScores or 5, whichever is the lesser
+    if (savedScores.length < 5) {
+      numToDisplay = savedScores.length;
+    }
+    for (i = 0; i < numToDisplay; i++) {
       const li = document.createElement("li");
-      li.textContent = tempHighScore + " by " + tempScoreName;
-      console.log("contents of list item:  " + li.textContent);
-      scoreList.appendChild(li);
+      li.textContent = savedScores[i].saveScore + " by " + savedScores[i].saveName;
+      li.classList.add("li-highscore");
+      lstScoreList.appendChild(li);
     }
   }
   return;
 }
 
 // This section not working correctly - will not remove "hide" class from highScoreScreen - works for other screens if substituted in
+// Function to display High Scores list
 function showHighScores(event) {
-  event.preventDefault();
-  console.log(`inshowHighScores`);
-  highScoreScrn.classList.remove("hide");
-  startScrn.classList.add("hide");
-  getHighScores();
+    
+  // The next line is not working
+  scrnHighScore.classList.remove("hide");
+
+  // From here down is working as planned
+  scrnStart.classList.add("hide");
+  // Retrieve high scores from local storage
+  getSavedScores();
+  // Sort scores and display the top 5 only
   renderHighScores();
-  
-  console.log(`savedScore length is ${savedScores.length}`);
-
-  goBackBtn.addEventListener("click", beginQuiz);
-  clearScoreBtn.addEventListener("click", clearHighScores);
-  
+  // If user presses Go Back button, reloads page
+  btnGoBack.addEventListener("click", function (event) {
+    window.location.reload();
+  });
+  // if User presses Clear Highscores, clears scores from local storage and reloads page
+  btnClearHighScores.addEventListener("click", function (event) {
+    localStorage.setItem('storedScores', "[]");
+    window.location.reload();
+  });
   return;
 }
 
-function returnStart(event) {
-  // submit initials and score to local storage
-  event.preventDefault();
-  
-  let tempName = initials.value.trim();
-  console.log(`name entered is ${tempName}`);
-  // create object with player name and score
-  const tempObj = {
-    saveName: tempName,
-    saveScore: finalScore
-  }
-  // add player name and score to end of savedScores array
-  if (tempName != "") {
-    savedScores.push(tempObj);
-  }
-
-  // write updated high score list to local storage
-  localStorage.setItem("storedScores", JSON.stringify(savedScores));  
-  console.log(`savedScores ${savedScores.length}`);
-  // reset savedScores to 0, to ensure only updated once
-  savedScores.length = 0;
-  tempName = "";
-   
-  // re-enable high score button
-  highscoreBtn.disabled = false;
-  // display start Page again
-  gameoverScrn.classList.add("hide");
-  startScrn.classList.remove("hide");
-  return;
-}
-
-function endQuiz() {
-  // display players final score
-  finalScoreText.textContent = finalScore;
-  // hide question screen
-  questionScrn.classList.add("hide");
-   // show Gameover screen - allows user to enter initials
-  gameoverScrn.classList.remove("hide");
-  submitBtn.addEventListener("click", returnStart);
-  return;
-}
-
-function clearHighScores(event) {
-  event.preventDefault;
-  console.log("inclearHighScores");
-
-  const clearEntry = [];
-  clearEntry.length = 0;
-  // set local storage to empty array to clear saved scores
-  localStorage.setItem("storedScores", JSON.stringify(clearEntry));
-  console.log("storage now contains:  " + JSON.parse(localStorage.getItem("storedScores")));
-  return;
-}
-
-function getHighScores() {
-  // get high scores from local storage
-  let tempScores = JSON.parse(localStorage.getItem("storedScores"));
- 
-  if (tempScores != null) {
-    // add each name/score object in storage to savedScores
-    for (i = 0; i < tempScores.length; i++) {
-      savedScores[i] = tempScores[i];
-    }
-  }
-  return;
-}
-
-function beginQuiz() {
-  // reset variables
-  questionResult.textContent = "";
-  initials.textContent = "";
-  // savedScores.length = 0;
-  
-  // disable High Scores button while quiz is in progress
-  highscoreBtn.disabled = true;
-  // hide startup screen
-  startScrn.classList.add("hide");
-  
-  // ensure timer reset
-  timeRemaining = 70;
-  timerText.textContent = timeRemaining;
-  // display question screen
-  questionScrn.classList.remove("hide");
-  questionIndex = 0;
-  // start timer
-  startTimer();
-  getQuestions(questionIndex);
-  return;
-}
-
-getHighScores();
-
+// Event listeners for buttons displayed on opening screen
 // When user clicks on Start, start the quiz by calling beginQuiz
-startBtn.addEventListener("click", beginQuiz);
+btnStart.addEventListener("click", beginQuiz);
 
 // When user clicks on High Scores, go to the highscores screen
-highscoreBtn.addEventListener("click", showHighScores);
-  
-  
-// pseudo code
-
-// play again button will take player back to the start screen
-// highscores screen will show a list of the highest scores in order from highest to lowest.  It will have a back button and a button to clear the high scores table
-
-// enter and save score in local storage
-
-// add event listener for high scores button - disable during question time
-
+btnHighScores.addEventListener("click", showHighScores);
